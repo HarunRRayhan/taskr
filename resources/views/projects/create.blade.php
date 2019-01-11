e274bfe1f9f49ce4fe93c2b576c90186bb9cab7e
Harun R Rayhan <harun.b13@gmail.com>
2019-01-11 17:57:32 +0600

    Strated Frontend w/ Tailwind

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,15 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Taskr</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
-</head>
-<body>
-<form method="POST" action="/projects" class="container" style="padding-top: 40px">
+@extends ('layouts.app')
+
+@section('content')
+<form method="POST" action="/projects">
 
     @csrf
 
@@ -34,9 +26,9 @@
     <div class="field">
         <div class="control">
             <button type="submit" class="button is-link">Create Project</button>
+            <a href="/projects">Cancel</a>
         </div>
     </div>
 
 </form>
-</body>
-</html>
+@endsection
